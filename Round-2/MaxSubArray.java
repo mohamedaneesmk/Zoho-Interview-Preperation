fc77eeb7a0c88f62c5fece1e11eb5832006c611a
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-28 17:43:07 +0530

    Added MaxSubArray.java Solution

--- Round-2/MaxSubArray.java
+++ Round-2/MaxSubArray.java
@@ -25,7 +25,7 @@ class MaxSubArray {
         int[] nums = {5, 4, -1, 7, 8};
         int result = findMaxSubArray(nums);
         System.out.println(result);
-    }
+    } 
 
     private static int findMaxSubArray(int[] nums) {
         int maxSum = nums[0];
