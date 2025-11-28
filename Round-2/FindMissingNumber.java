0e89e84c32936f2b48b705c83673d8196c6c1a62
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-11-28 17:34:30 +0530

    Added FindMissingNumber.java Solution

--- Round-2/FindMissingNumber.java
+++ Round-2/FindMissingNumber.java
@@ -3,7 +3,7 @@ public class FindMissingNumber {
         int[] arr = {0, 1, 2, 4, 5, 6, 7, 8, 9, 10};
         int result = findMissingNumber(arr);
         System.out.println("The missing number is: " + result);
-    }
+    } 
 
     private static int findMissingNumber(int[] arr) {
         int max = 0;
