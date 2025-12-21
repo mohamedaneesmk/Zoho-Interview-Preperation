be61d6dba82d857665fa4859c97596705122de58
mohamedaneesmk <21ec267@students.stjosephs.ac.in>
2025-12-21 21:01:53 +0530

    Fibonacci Recursion

---
+++ Round-2/ClimbingStairs.java
@@ -0,0 +1,15 @@
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int num = 2;
+        int result = findClimbingStairs(num);
+
+        System.out.println(result);
+    }
+
+    private static int findClimbingStairs(int num) {
+        if(num == 1) return 1;
+        if(num == 2) return 2;
+
+        
+    }
+}
