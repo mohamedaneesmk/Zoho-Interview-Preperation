0cd318f9626a8d368453a5a9f0df976a40e7893e
mohamedaneesmk <mohamedaneesmk@gmail.com>
2026-01-04 11:39:51 +0530

    Print the Sub Array | LeetCode #53

--- Round-2/MaxSubArray.java
+++ Round-2/MaxSubArray.java
@@ -41,7 +41,7 @@
 }*/
 
 // To Print Subarray as well
-public class MaxSubArray {
+public class MaxSubArray { 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         // int[] nums = {1};
